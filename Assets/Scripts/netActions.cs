6ef7d0eb21b9d3bc90dee25f545d00233717e765
clarence112 <clarencepacelli@gmail.com>
2022-04-13 17:13:43 -0400

    no message

---
+++ Assets/Scripts/netActions.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class netActions:MonoBehaviour {
+
+    public static bool playmode;
+    public NetworkManager nm;
+    public NetworkTransport nt;
+
+    void Start() {
+
+    }
+
+
+    void Update() {
+
+    }
+}
