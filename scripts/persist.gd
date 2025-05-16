f1fe29ba11282eb007f62086f255a6f85313c569
clarence112 <clarencepacelli@gmail.com>
2025-05-16 15:33:27 -0400

    4.3?

--- scripts/persist.gd
+++ scripts/persist.gd
@@ -2,9 +2,9 @@ extends Node
 
 
 enum Char {
-    Clarence,
-    Epsilon,
-    Request,
+	Clarence,
+	Epsilon,
+	Request,
 }
 
 
@@ -16,41 +16,41 @@ var _charsel:OptionButton
 
 @rpc("authority", "call_remote", "reliable")
 func setid(i:int):
-    id = i
+	id = i
 
 
 @rpc("any_peer", "call_remote", "reliable")
 func akchar():
-    if Statics.multiplayer.is_server():
-        startgame.rpc()
+	if Statics.multiplayer.is_server():
+		startgame.rpc()
 
 
 @rpc("authority", "call_remote", "reliable")
 func setchar(char:Char):
-    MultiManager.dlog(Char.find_key(char))
-    if !Statics.multiplayer.is_server():
-        akchar.rpc()
+	MultiManager.dlog(Char.find_key(char))
+	if !Statics.multiplayer.is_server():
+		akchar.rpc()
 
 
 @rpc("authority", "call_local", "reliable")
 func startgame():
-    get_child(0).hide()
-    get_tree().change_scene_to_file("res://assets/world.tscn")
+	get_child(0).hide()
+	get_tree().change_scene_to_file("res://assets/world.tscn")
 
 
 @rpc("any_peer", "call_remote", "reliable")
 func negotiate_char(char:Char):
-    if Statics.multiplayer.is_server():
-        assert(char != Char.Request, "Client is requesting character!")
-        if char == Char.Clarence:
-            setchar(Char.Epsilon)
-            setchar.rpc(Char.Clarence)
-        else:
-            setchar(Char.Clarence)
-            setchar.rpc(Char.Epsilon)
-    else:
-        assert(char == Char.Request, "Server already knows character but is trying to negotiate!")
-        var tosel:Char = _charsel.selected  as Char
-        if tosel == Char.Request:
-            tosel = randi_range(0, 1) as Char
-        negotiate_char.rpc(tosel)
+	if Statics.multiplayer.is_server():
+		assert(char != Char.Request, "Client is requesting character!")
+		if char == Char.Clarence:
+			setchar(Char.Epsilon)
+			setchar.rpc(Char.Clarence)
+		else:
+			setchar(Char.Clarence)
+			setchar.rpc(Char.Epsilon)
+	else:
+		assert(char == Char.Request, "Server already knows character but is trying to negotiate!")
+		var tosel:Char = _charsel.selected  as Char
+		if tosel == Char.Request:
+			tosel = randi_range(0, 1) as Char
+		negotiate_char.rpc(tosel)
--- scripts/titleControl.gd
+++ scripts/titleControl.gd
@@ -3,21 +3,21 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    Persist._charsel = $CenterContainer/TabContainer/Join/HBoxContainer/OptionButton
+	Persist._charsel = $CenterContainer/TabContainer/Join/HBoxContainer/OptionButton
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    pass
+	pass
 
 
 func _on_host_pressed():
-    var error = MultiManager.mhost()
-    if error == OK:
-        hide()
-    else:
-        print(error)
+	var error = MultiManager.mhost()
+	if error == OK:
+		hide()
+	else:
+		print(error)
 
 
 func _on_join_pressed():
-    MultiManager.mjoin()
+	MultiManager.mjoin()
